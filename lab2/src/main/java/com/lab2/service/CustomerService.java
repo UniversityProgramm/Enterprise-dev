4661f4007cc448a1b32e2fbc6c5b6d1a66045559
Gordeev203 <gordeev.stas2004.s@mail.ru>
2026-03-28 21:47:42 +0300

    Lab2

---
+++ lab2/src/main/java/com/lab2/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.lab2.service;
+
+import com.lab2.entity.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerService {
+
+    List<Customer> getAllCustomers();
+
+    Optional<Customer> getCustomerById(Long id);
+
+    Customer createCustomer(Customer customer);
+
+    Customer updateCustomer(Long id, Customer customer);
+
+    void deleteCustomer(Long id);
+}
